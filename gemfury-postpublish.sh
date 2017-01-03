8579206408b9e3070b90e645cd07c93cf0aed9c0
Julian <julianbrendl@gmail.com>
2017-01-03 13:52:11 +0100

    added publish script

---
+++ gemfury-postpublish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Setting npm registry back to default.."
+npm config set registry https://registry.npmjs.org/
+echo "Done."
---
+++ gemfury-prepublish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Building project.."
+npm start
+echo "Done."
+
+echo "Setting npm registry to GemFury.."
+npm config set registry https://npm-proxy.fury.io/sayhi-ai/
+echo "Done."
+
+echo "Logging in.."
+npm adduser
+echo "Done."
